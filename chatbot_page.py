eb6a43b3d5071d4d08cb8fcd97503c4fb0ccbedf
qxdo <qxdo123@gmail.com>
2023-05-13 10:18:17 +0800

    update data

--- chatbot_page.py
+++ chatbot_page.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', name='test')
 
+@app.route('/api/chat', methods=['POST'])
+def api_message():
+    data = request.get_json()
+    print(data['message'])
+    # 在这里处理消息，例如发送电子邮件或保存到数据库
+    return jsonify({'code': 0, 'message': 'success', 'data': '测试12345676'})
+
 if __name__ == '__main__':
     app.run(debug=True)
+
