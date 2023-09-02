df7c1272a4f5798b92252d8c401b1bb6e6fb5474
qxdo <qxdo123@gmail.com>
2023-09-02 15:30:34 +0800

    update data

---
+++ prepare_data/test.py
@@ -0,0 +1,10 @@
+
+class Test:
+    def init(self):
+        print("ok test")
+    def __matmul__(self, other):
+        print("1234556")
+    def self1(self):
+        print("self 1")
+    def self2(self):
+        print("self 2")
